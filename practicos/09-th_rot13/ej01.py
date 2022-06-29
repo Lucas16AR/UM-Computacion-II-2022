eb3742bbe39af60d063152b300ce6198fd8c88d5
Lucas16AR <luqui0008@gmail.com>
2022-06-29 20:31:55 -0300

    tp 09 arrancando

---
+++ practicos/09-th_rot13/ej01.py
@@ -0,0 +1,31 @@
+'''
+Generar dos hilos usando threading
+Un hilo lee desde stdin un texto del usuario (escribir en mecanismo IPC)
+Hilo 2 lee desde mecanismo IPC, encripta con algorithm rot13, almacena en cola de mensaje
+Hijo 1 lee la cola de mensajes, mostrar contenido por pantalla
+Entregar antes de viernes
+'''
+
+
+
+import threading as th , codecs, sys, queue, os
+
+def thread_1():
+
+
+
+    pass
+
+def thread_2():
+
+
+
+    pass
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- tps
+++ tps
@@ -5,5 +5,5 @@
 #done: Procesos - [pipe]
 #done: Sincronizacion y shm con mmap - [mmap_sig]
 #done: Rot13 con multiprocessing - [mp_rot13]
-Calculo matricial - [matriz_pool]
+#done: Calculo matricial - [matriz_pool]
 Rot13 con threading - [th_rot13]
