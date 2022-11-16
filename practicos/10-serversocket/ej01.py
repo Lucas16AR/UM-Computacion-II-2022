5ea22252f5ee20d5286a93e83ef3737e2421b044
Lucas16AR <luqui0008@gmail.com>
2022-11-16 09:27:39 -0300

    serversocket

--- practicos/10-serversocket/ej01.py
+++ practicos/10-serversocket/ej01.py
@@ -1,68 +1,31 @@
-import getopt, threading, socket, subprocess, socketserver, os, sys
+import socket, getopt, sys
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    
-    def handle(self, client):
-        addr = client
-        while True:
-            data = self.request.recv(1024)
-            message = data.decode("ascii")
-            if data.decode('ascii') == 'exit':
-                client.send('Terminado'.encode('ascii'))
-                break
-            print("Direccion: %s" % str(addr))
-            print("Recibido:" + data.decode('ascii'))
-            result = subprocess.Popen([data], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            stdout, stderr = result.communicate()
-            if stdout != "":
-                message = "Ok\n" + stdout
-            elif stderr != "":
-                message = "Error\n" + stderr
+try:
+    opt,arg = getopt.getopt(sys.argv[1:], 'h:p:')
+    if len(opt) != 2:
+        print("Ingrese bien la cantidad de argumentos")
+        exit()
+except getopt.GetoptError as error:
+    print(f'Ha habido un error: {error}')
+    exit()
 
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
+for (op,ar) in opt:
+    if op == '-h':
+       host = str(ar)
+    if op == '-p':
+        port = int(ar)
 
-class ForkedTCPServer(socketserver.ForkingMixIn, socketserver.TCPServer):
-    pass
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+print("Ingrese un comando")
 
-def main():
-    try:
-        (opt, ar) = getopt.getopt(sys.argv[1:], 'm:')
-    except:
-        sys.exit(1)
-
-    for (op, ar) in opt:
-        if op == ['-m']:
-            opt = ar
-        else:
-            sys.exit(2)
-    address = ('localhost', 8888)
-    if opt == 'p':
-        server = ForkedTCPServer(address, MyTCPHandler)
-        print("Servidor en bucle corriendo en proceso: ", os.getpid())
-    elif opt == 'h':
-        server = ThreadedTCPServer(address, MyTCPHandler)
-    ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client = ss.accept()
-    th = threading.Thread(target=server.serve_forever, args=(client,))
-    th.setDaemon(True)
-    th.start()
-    ip, port = ('localhost', 0)
-    ss.connect((ip, port))
-    addr = client
-    
-    if opt == 't':
-        print("Servidor en bucle corriendo en hilo: ", th.getName())
-
-    while True:
-        msg = input("Ingrese un mensaje: ")
-        ss.send(msg.encode('ascii'))
-        data = ss.recv(1024).decode('ascii')
-        print(data)
-        if msg == 'exit':
-            server.shutdown()
-            ss.close()
-            server.socket.close()
-
-if __name__ == "__main__":
-    main()
+while True:
+    command = input('> ')
+    if len(command) == 0 or command == "Adios":
+        print("Saliendo")
+        s.send(command.encode("ascii"))
+        break
+    else:
+        s.send(command.encode("ascii"))
+        recv = str(s.recv(1024).decode("ascii"))
+        print(recv)
--- practicos/10-serversocket/ej02.py
+++ practicos/10-serversocket/ej02.py
@@ -1,28 +1,56 @@
-import socket, getopt, sys
+import socketserver, subprocess, getopt, sys
 
 try:
-    opt,arg = getopt.getopt(sys.argv[1:], "h:p:")
-    if len(opt) != 2:
-        print ("Ingrese bien la cantidad de argumentos...")
+    opt,arg = getopt.getopt(sys.argv[1:], 'h:p:a:')
+    if len(opt) != 3:
+        print("Ingrese bien la cantidad de argumentos")
         exit()
 except getopt.GetoptError as error:
-    print (f"Error: %s" % error)
+    print(f'Ha habido un error: {error}')
     exit()
 
 for (op,ar) in opt:
     if op == '-h':
-        host = str(ar)
+       host = str(ar)
     if op == '-p':
         port = int(ar)
+    if op == '-a':
+        args = str(ar)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-print("Ingrese un comando")
+class Thread(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
 
-while True:
-    command = input("Ingrese un comando")
-    s.send(command.encode())
-    if command == 'exit':
-        break
-    result = s.recv(1024)
-    print(result.decode())
+class Process(socketserver.ForkingMixIn, socketserver.TCPServer):
+    pass
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        while True:
+            data = self.request.recv(1024).strip()
+            if len(data) == 0 or data == "Adios":
+                print(f"Cliente offline {self.client_address[0]}")
+                exit(0)
+            command = subprocess.Popen([data], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            stdout, stderr = command.communicate()
+            if command.returncode == 0:
+                ans = "Exito \n"+ stdout
+                print(f'El comando {data.decode("ascii")} se ejecuto correctamente')
+            else:
+                ans = "Error \n"+ stderr
+                print(f'El comando {data.decode("ascii")} no se ejecuto correctamente')
+
+            self.request.send(ans.encode('ascii'))
+
+if __name__ == "__main__":
+    socketserver.TCPServer.allow_reuse_address = True
+    if args == "p":
+        server = Process((host, port), MyTCPHandler)
+        print(f"Servidor con procesos (Port= {port})")
+        server.serve_forever()
+    elif args == "t":
+        server = Thread((host, port), MyTCPHandler)
+        print(f"Servidor con hilos (Port= {port})")
+        server.serve_forever()
+    else:
+        'Error: Unknown'
